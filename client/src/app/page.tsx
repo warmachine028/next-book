91f177b714255a9e8a6a7617e88589d9c631b5a9
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-08-24 23:18:56 +0530

    feat: added components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export const Home = () => {
+const Home = () => {
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
