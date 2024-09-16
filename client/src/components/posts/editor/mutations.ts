0c4db41baf5407e830483e100c0b73bd6a87b065
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-17 03:44:19 +0530

    feat: optimistic updates for posts feed

--- client/src/components/posts/editor/mutations.ts
+++ client/src/components/posts/editor/mutations.ts
@@ -2,16 +2,25 @@ import { useToast } from '@/components/ui/use-toast'
 import { InfiniteData, QueryFilters, useMutation, useQueryClient } from '@tanstack/react-query'
 import { createPost } from './actions'
 import { PostsPage } from '@/types'
+import { useSession } from '@/hooks'
 
 export const useSubmitPostMutation = () => {
 	const { toast } = useToast()
 
 	const queryClient = useQueryClient()
-
+	const { user } = useSession()
 	const mutation = useMutation({
 		mutationFn: createPost,
 		onSuccess: async (newPost) => {
-			const queryFilter: QueryFilters = { queryKey: ['post-feed', 'for-you'] }
+			const queryFilter = {
+				queryKey: ['post-feed'],
+				predicate(query) {
+					return (
+						query.queryKey.includes('for-you') ||
+						(query.queryKey.includes('user-posts') && query.queryKey.includes(user.id))
+					)
+				}
+			} satisfies QueryFilters
 			await queryClient.cancelQueries(queryFilter)
 			queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(queryFilter, (oldData) => {
 				const firstPage = oldData?.pages[0]
@@ -31,7 +40,7 @@ export const useSubmitPostMutation = () => {
 			queryClient.invalidateQueries({
 				queryKey: queryFilter.queryKey,
 				predicate(query) {
-					return !query.state.data
+					return queryFilter.predicate(query) && !query.state.data
 				}
 			})
 			toast({
