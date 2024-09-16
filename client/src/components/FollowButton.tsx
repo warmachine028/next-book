2d870bf0926a25818858ad413dbca2825605bb1e
warmachine028 <75939390+warmachine028@users.noreply.github.com>
2024-09-17 01:13:46 +0530

    feat: optimistic updates for following

--- client/src/components/FollowButton.tsx
+++ client/src/components/FollowButton.tsx
@@ -3,7 +3,7 @@
 import { useFollowerInfo } from '@/hooks'
 import { FollowerInfo } from '@/types'
 import { useToast } from './ui/use-toast'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { QueryKey, useMutation, useQueryClient } from '@tanstack/react-query'
 import { Button } from './ui/button'
 import { kyInstance } from '@/lib/ky'
 
@@ -16,11 +16,31 @@ const FollowButton = ({ userId, initialState }: FollowButtonProps) => {
 	const { toast } = useToast()
 	const queryClient = useQueryClient()
 	const { data } = useFollowerInfo(userId, initialState)
+	const queryKey: QueryKey = ['follower-info', userId]
+
 	const { mutate } = useMutation({
 		mutationFn: () =>
 			data.isFollowedByUser ?
 				kyInstance.delete(`/api/users/${userId}/followers`)
-			:	kyInstance.post(`/api/users/${userId}/followers`)
+			:	kyInstance.post(`/api/users/${userId}/followers`),
+		onMutate: async () => {
+			await queryClient.cancelQueries({ queryKey })
+
+			const previousState = queryClient.getQueryData<FollowerInfo>(queryKey)
+			queryClient.setQueryData<FollowerInfo>(queryKey, () => ({
+				followers: (previousState?.followers || 0) + (previousState?.isFollowedByUser ? -1 : +1),
+				isFollowedByUser: !previousState?.isFollowedByUser
+			}))
+			return { previousState }
+		},
+		onError: (error, variables, context) => {
+			queryClient.setQueryData(queryKey, context?.previousState)
+			console.error(error)
+			toast({
+				variant: 'destructive',
+				description: 'Something went wrong. Please try again.'
+			})
+		}
 	})
 
 	return (
