9a5012c82a923a5dc3f03ed5a6c7779ecb9d03f6
Pritam Kundu <warmachine028@users.noreply.github.com>
2024-11-11 21:53:27 +0530

    feat: bookmarks page

--- client/src/app/(main)/bookmarks/page.tsx
+++ client/src/app/(main)/bookmarks/page.tsx
@@ -12,10 +12,10 @@ const Bookmarks = () => {
 			<Menubar className="hidden h-fit flex-none space-y-3 rounded-md bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
 			<div className="flex w-full min-w-0 gap-5">
 				<div className="w-full min-w-0 space-y-5">
-					<div className="rounded-sm bg-card p-5 shadow-sm">
-						<h1 className="text-center text-2xl font-bold">Bookmarks</h1>
+					<div className="rounded-md bg-card p-5 shadow-sm">
+						<h1 className="text-center text-xl font-bold">Bookmarks</h1>
 					</div>
-					<ScrollArea className="h-[calc(100vh-300px)] min-h-96 min-w-full">
+					<ScrollArea className="h-[calc(100vh-300px)] min-h-96 min-w-full rounded-md">
 						<BookmarksFeed />
 					</ScrollArea>
 				</div>
